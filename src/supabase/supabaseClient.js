00368556ab02b3ac0bbcdf4d98caa9b8a40bf9c5
supremeamerapplication <supremeamerapplication@gmail.com>
2026-02-02 22:13:00 -0800

    Update supabaseClient.js

--- src/supabase/supabaseClient.js
+++ src/supabase/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://your-project.supabase.co'
-const supabaseAnonKey = 'your-anon-key'
+const supabaseUrl = 'https://xlqacsxmqahizlklvhfp.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhscWFjc3htcWFoaXpsa2x2aGZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwOTQxNTAsImV4cCI6MjA4NTY3MDE1MH0.DAYvGHmia5zwiEvK0W-oGDO9H4fDA96CLhW_DhIlQEo'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
